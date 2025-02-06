13c7b5ed6022a236e2811748f75341d1a4b8508f
Denis-create-boop <den_1994_2015@mail.ru>
2025-02-06 11:21:46 +0300

    created class Operation for worck with two numbers

---
+++ main.py
@@ -0,0 +1,22 @@
+# импортируем класс для работы с числами
+from operations import Operation
+
+# создаем обьект класса
+operation = Operation()
+
+# функция для вычисления выражения
+def calc(x, y, operator):
+    pass
+
+# функция провеки данных
+def check(s, op):
+    pass
+
+# основная функция программы
+def main():
+    # принимает выражение от пользова
+    s = input("Введите выражение: ").split()
+
+
+if __name__ == "__main__":
+    main()
---
+++ operations.py
@@ -0,0 +1,30 @@
+# создал простой класс для работы с двумя числами
+
+class Operation:
+    """Класс для работы с числами, умеет складывать, вычитать, делить, умножать и находить остаток 
+        от деления двух чисел"""
+    # иницилизируем два числа
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # функция для сложения двух чисел
+    def addition(self):
+        return self.x + self.y
+
+    # функция для вычитания одного числа из другого
+    def subtraction(self):
+        return self.x - self.y
+    
+    # функция для деления одного числа на дрогое
+    def division(self):
+        return self.x / self.y
+    
+    # функция для умножения двух чисел
+    def multiplication(self):
+        return self.x * self.y
+    
+    # функция для нахождения остатка от деления
+    def reminder(self):
+        return self.x % self.y
+    
